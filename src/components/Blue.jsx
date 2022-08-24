ca65c97b85187ccfa30d884303a7d3cf6024bada
이상철 <zlrz6706@gmail.com>
2022-08-24 23:06:09 +0900

    Commit before eject

---
+++ src/components/Blue.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './blue.scss'
+
+const Blue = () => {
+  return (
+    <div className="blue">
+      Blue
+    </div>
+  );
+};
+
+export default Blue;
---
+++ src/components/Red.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./red.scss";
+
+const Red = () => {
+  return (
+    <div className="red">
+      Red
+    </div>
+  );
+};
+
+export default Red;
